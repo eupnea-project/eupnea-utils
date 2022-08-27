0568326de1f8527dbbd536b50a95eff13c69f4b2
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-08-27 16:08:06 +0100

    Actually rewrite script in python

--- scripts/sof-setup-audio.py
+++ scripts/sof-setup-audio.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
-import subprocess as sp
 from pathlib import Path
+import os
+import sys
+import shutil
 
 
 def process_args():
@@ -18,14 +20,15 @@ def process_args():
 
 def download_files() -> None:
     print("\033[94m" + "Removing old files" + "\033[0m")
-    sp.run(["sudo", "rm", "-rf", str(Path.home()) + "/.config/eupnea/audio"])
-    sp.run(["mkdir", "-p", str(Path.home()) + "/.config/eupnea/audio"])
-    sp.run("rm -rf /tmp/eupnea* /tmp/sof*", shell=True)
+    os.rmdir(home_path)
+    os.rmdir("/tmp/eupnea-audio")
+    os.rmdir("/tmp/sof-audio")
+    Path(home_path).mkdir(parents=True, exist_ok=True)
     print("\033[94m" + "Downloading files from github" + "\033[0m")
-    sp.run("git clone --depth 1 https://github.com/eupnea-linux/python-scripts /tmp/eupnea-audio", shell=True)
-    sp.run("git clone --depth 1 https://github.com/thesofproject/sof-bin /tmp/sof-audio", shell=True)
+    os.system("git clone --depth 1 https://github.com/eupnea-linux/python-scripts /tmp/eupnea-audio")
+    os.system("git clone --depth 1 https://github.com/thesofproject/sof-bin /tmp/sof-audio")
     print("\033[94m" + "Copying files from tmp" + "\033[0m")
-    sp.run("cp /tmp/eupnea-audio/configs/* " + home_path, shell=True)
+    shutil.copytree("cp /tmp/eupnea-audio/configs/", home_path, dirs_exist_ok=True)
 
 
 def install_pa(local_files: bool, sof_version) -> None:
@@ -33,34 +36,41 @@ def install_pa(local_files: bool, sof_version) -> None:
         sof_version = "v2.2.x"
     print("\033[95m" + "Installing pulseaudio" + "\033[0m")
     print("\033[94m" + "Removing old files" + "\033[0m")
-    sp.run("sudo rm -f /etc/systemd/system/alsa-reload.service", shell=True)
-    sp.run("sudo rm -f /etc/pulse/default.pa", shell=True)
-    sp.run("sudo rm -f /etc/modprobe.d/alsa-breath.conf", shell=True)
-    sp.run("sudo rm -f /etc/asound.conf", shell=True)
+    os.remove("/etc/systemd/system/alsa-reload.service")
+    os.remove("/etc/pulse/default.pa")
+    os.remove("/etc/modprobe.d/alsa-breath.conf")
+    os.remove("/etc/asound.conf")
     if not local_files:
-        sp.run(["sudo", "rm", "-f", "/lib/firmware/intel/sof*"])
+        # remove all old sof folders + files
+        for directory in Path("/lib/firmware/intel/").glob("sof*"):
+            try:
+                directory.rmdir()
+            except NotADirectoryError:
+                directory.unlink()
         print("\033[95m" + "Installing sof-audio" + "\033[0m")
-        sp.run("sudo cp -r /tmp/sof-audio/" + sof_version + "/sof" + " /lib/firmware/intel/sof",
-               shell=True)
-        sp.run("sudo cp -r /tmp/sof-audio/"
-               + sof_version + "/sof-tplg" + " /lib/firmware/intel/sof-tplg",
-               shell=True)
-        sp.run("sudo cp -r /tmp/sof-audio/" + sof_version + "/tools" + "* /usr/local/bin/",
-               shell=True)
+        shutil.copytree("/tmp/sof-audio/" + sof_version + "/sof", "/lib/firmware/intel/sof")
+        shutil.copytree("/tmp/sof-audio/" + sof_version + "/sof-tplg", "/lib/firmware/intel/sof-tplg")
+        shutil.copytree("/tmp/sof-audio/" + sof_version + "/tools" + sof_version[:sof_version.rfind(".")],
+                        "/usr/local/bin/")
     print("\033[94m" + "Installing audio services" + "\033[0m")
-    sp.run("sudo cp " + home_path + "alsa-reload.service /etc/systemd/system/", shell=True)
-    sp.run("sudo systemctl enable alsa-reload", shell=True)
+    shutil.copyfile(home_path + "alsa-reload.service", "/etc/systemd/system/")
+    os.system("sudo systemctl enable alsa-reload")
     print("\033[94m" + "Copying pa config" + "\033[0m")
-    sp.run("sudo cp " + home_path + "default.pa /etc/pulse/", shell=True)
-    print("\033[94m" + "Config old drivers" + "\033[0m")
-    sp.run("sudo cp " + home_path + "alsa-breath.conf /etc/modprobe.d/", shell=True)
+    shutil.copyfile(home_path + "default.pa", "/etc/pulse/")
+    print("\033[94m" + "Blacklising old drivers" + "\033[0m")
+    shutil.copyfile(home_path + "alsa-breath.conf" "/etc/modprobe.d/")
     print("\033[94m" + "Copying asound.conf" + "\033[0m")
-    sp.run("sudo cp " + home_path + "asound.conf /etc/", shell=True)
+    shutil.copyfile(home_path + "asound.conf" "/etc/")
 
 
 if __name__ == "__main__":
+    # Elevate script to root
+    euid = os.geteuid()
+    if euid != 0:
+        args = ['sudo', sys.executable] + sys.argv + [os.environ]
+        os.execlpe('sudo', *args)
     args = process_args()
-    home_path = str(Path.home()) + "/.config/eupnea/audio/"
+    home_path = str(Path.home()) + "/.config/eupnea/audio"
     if not args.local_files:
         download_files()
     if not args.no_pa:
