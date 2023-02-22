ce62b06197824c154e8b6fefa9385b1283a370c0
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-02-22 18:10:43 +0100

    Remove sudo

--- system-scripts/dnf-autoremove-kernels.sh
+++ system-scripts/dnf-autoremove-kernels.sh
@@ -4,8 +4,8 @@
 
 # one of the following commands will fail due to dnf preventing the removal of the currently running kernel
 # this is fine, we just want to remove the other kernel
-sudo dnf remove -y eupnea-mainline-kernel || true
-sudo dnf remove -y eupnea-chromeos-kernel || true
+dnf remove -y eupnea-mainline-kernel || true
+dnf remove -y eupnea-chromeos-kernel || true
 
 # delete systemd service
 rm /usr/lib/systemd/system/eupnea-kernel-autoremove.service
