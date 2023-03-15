ef2e3165ec5fb720040a6a586858f85a0981d5fa
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-03-15 21:22:24 +0100

    Fix echo commands + dont use sudo

--- system-scripts/fix-touchscreen.sh
+++ system-scripts/fix-touchscreen.sh
@@ -4,12 +4,12 @@
 # SOURCE: https://github.com/GalliumOS/galliumos-distro/issues/606#issuecomment-1009236456
 if [ -d /sys/bus/i2c/devices/i2c-SYTS7817:00 ]; then
   echo "Linking i2c_hid_acpi to SYTS7817 touchscreen device"
-  echo -n \"i2c-SYTS7817:00\" >/sys/bus/i2c/drivers/i2c_hid_acpi/bind
+  echo "i2c-SYTS7817:00" >/sys/bus/i2c/drivers/i2c_hid_acpi/bind
 fi
 # Set correct module for goodix touchscreens
 if [ -d /sys/bus/i2c/devices/i2c-GDIX0000:00 ]; then
   echo "Linking i2c_hid_acpi to GDIX0000 touchscreen device"
-  echo -n \"i2c-GDIX0000:00\" >/sys/bus/i2c/drivers/i2c_hid_acpi/bind
+  echo "i2c-GDIX0000:00" >/sys/bus/i2c/drivers/i2c_hid_acpi/bind
 fi
 
 # Reload the elants_i2c driver
@@ -18,7 +18,7 @@ fi
 if lsmod | grep -qw elants_i2c; then
   # Reload the module
   echo "Reloading elants_i2c driver"
-  sudo modprobe -r elants_i2c
-  sudo modprobe elants_i2c
+  modprobe -r elants_i2c
+  modprobe elants_i2c
 fi
 
