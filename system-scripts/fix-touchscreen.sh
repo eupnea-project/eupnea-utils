13ed0314e766c8a3698c81843f16ee84d32acd60
WeirdTreeThing <bradyn127@protonmail.com>
2023-03-15 21:29:12 -0400

    Move elan touchscreen fix above fixes for i2c_hid_acpi

--- system-scripts/fix-touchscreen.sh
+++ system-scripts/fix-touchscreen.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Reload the elants_i2c driver
+# This is mainline specific, as it works fine on chromeos kernels
+# Check if module is loaded
+if lsmod | grep -qw elants_i2c; then
+  # Reload the module
+  echo "Reloading elants_i2c driver"
+  modprobe -r elants_i2c
+  modprobe elants_i2c
+fi
+
 # link the i2c_hid_acpi driver to the SYTS7817 touchscreen device
 # SOURCE: https://github.com/GalliumOS/galliumos-distro/issues/606#issuecomment-1009236456
 if [ -d /sys/bus/i2c/devices/i2c-SYTS7817:00 ]; then
@@ -11,14 +21,3 @@ if [ -d /sys/bus/i2c/devices/i2c-GDIX0000:00 ]; then
   echo "Linking i2c_hid_acpi to GDIX0000 touchscreen device"
   echo "i2c-GDIX0000:00" >/sys/bus/i2c/drivers/i2c_hid_acpi/bind
 fi
-
-# Reload the elants_i2c driver
-# This is mainline specific, as it works fine on chromeos kernels
-# Check if module is loaded
-if lsmod | grep -qw elants_i2c; then
-  # Reload the module
-  echo "Reloading elants_i2c driver"
-  modprobe -r elants_i2c
-  modprobe elants_i2c
-fi
-
